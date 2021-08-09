a17b20c6275c9c7704b7621d161dc18af49bbdcd
zjy <zhaojinyun15@outlook.com>
2021-08-09 22:09:39 +0800

    merge

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def create_random_list(list_len):
     """
     if list_len < 0:
         raise Exception('params error!')
-    if list_len > 1000000:
+    if list_len > 100000:
         raise Exception('too long list!')
     max_num = list_len * 10
     random_list = []
@@ -23,10 +23,14 @@ def create_random_list(list_len):
 
 
 if __name__ == '__main__':
-    random_list = create_random_list(10000)
+    random_list = create_random_list(100000)
     # print(random_list)
     start_time = time.time()
-    permutation.insertion(random_list)
+
+    # permutation.bubble(random_list)
+    # permutation.insertion(random_list)
+    permutation.merge(random_list, 0, len(random_list))
+
     end_time = time.time()
     print(f'takes time: {(end_time - start_time) * 1000} ms')
     # print(random_list)
--- permutation.py
+++ permutation.py
@@ -36,3 +36,39 @@ def insertion(origin_list):
             j -= 1
         origin_list[j] = temp
     return origin_list
+
+
+def merge(origin_list, begin_index, end_index):
+    """
+    NlogN
+    :param origin_list:
+    :return:
+    """
+    if begin_index >= end_index:
+        return origin_list
+    half_index = (begin_index + end_index) // 2
+    # do left
+    merge(origin_list, begin_index, half_index)
+    # do right
+    merge(origin_list, half_index + 1, end_index)
+    # merge left and right
+    left_index, right_index, origin_index = 0, 0, begin_index
+    left_list = origin_list[begin_index: half_index + 1].copy()
+    right_list = origin_list[half_index + 1: end_index + 1].copy()
+    while left_index < len(left_list) and right_index < len(right_list):
+        if left_list[left_index] <= right_list[right_index]:
+            origin_list[origin_index] = left_list[left_index]
+            left_index += 1
+        else:
+            origin_list[origin_index] = right_list[right_index]
+            right_index += 1
+        origin_index += 1
+    while left_index < len(left_list):
+        origin_list[origin_index] = left_list[left_index]
+        left_index += 1
+        origin_index += 1
+    while right_index < len(right_list):
+        origin_list[origin_index] = right_list[right_index]
+        right_index += 1
+        origin_index += 1
+    return origin_list

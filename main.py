2bda32f674acbfd5a8378d8c3fd24d08036203b1
zjy <zhaojinyun15@outlook.com>
2021-08-15 23:13:13 +0800

    add check_permutation function

--- main.py
+++ main.py
@@ -22,6 +22,23 @@ def create_random_list(list_len):
     return random_list
 
 
+def check_permutation(unchecked_list, order='asc'):
+    """
+    检查list是否已排序
+    :param unchecked_list:
+    :param order:
+    :return:
+    """
+    if order not in ['asc', 'desc']:
+        raise Exception('order param should be asc or desc!')
+    for i in range(len(unchecked_list)):
+        if i + 1 < len(unchecked_list):
+            if (order == 'asc' and unchecked_list[i] > unchecked_list[i + 1]) or \
+                    (order == 'desc' and unchecked_list[i] < unchecked_list[i + 1]):
+                return False
+    return True
+
+
 if __name__ == '__main__':
     random_list = create_random_list(100000)
     # print(random_list)
@@ -36,3 +53,4 @@ if __name__ == '__main__':
     end_time = time.time()
     print(f'takes time: {(end_time - start_time) * 1000} ms')
     # print(random_list)
+    print(check_permutation(random_list))
